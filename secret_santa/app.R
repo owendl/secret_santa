3f07995144a07f4720c7e9b529a126415c2a43f1
Drew Owen <owen.drew.l@gmail.com>
2021-12-24 12:01:19 -0500

    added some default text inputs, creates all of the combos of name_combos that aren't themselves.

--- secret_santa/app.R
+++ secret_santa/app.R
@@ -3,6 +3,8 @@ library(jsonlite)
 library(mailR)
 smtp_creds = read_json("smtp_creds.json")
 # Define UI for application that draws a histogram
+
+
 ui <- fluidPage(
     # Application title
     titlePanel("Secret Santa by Drew"),
@@ -18,14 +20,32 @@ ui <- fluidPage(
         )       
         
         ,column(2,
-               tags$div(id = 'names')    
+               tags$div(id = 'names'),
+               wellPanel(
+                   textInput("nameA", label = "default names", value="drew")
+                   ,textInput("nameB", label = "default names", value="amelia")
+                   ,textInput("nameC", label = "default names", value="chris")
+                   ,textInput("nameD", label = "default names", value="amanda")
+               )
         )
         
         ,column(4,
-           tags$div(id = 'emails')    
+           tags$div(id = 'emails'),
+           wellPanel(
+               textInput("emailA", label = "default emails", value="drduber@gmail.com")
+               ,textInput("emailB", label = "default emails", value="atgodfrey@gmail.com")
+               ,textInput("emailC", label = "default emails", value="dowen3@gatech.edu")
+               ,textInput("emailD", label = "default emails", value="owen.drew.l@gmail.com")
+           )    
         )
         ,column(3,
-                tags$div(id = 'excludes')    
+                tags$div(id = 'excludes'),
+                wellPanel(
+                    textInput("excludeA", label = "default", value="amelia")
+                    ,textInput("excludeB", label = "default", value="drew")
+                    ,textInput("excludeC", label = "default", value="amanda")
+                    ,textInput("excludeD", label = "default", value="chris")
+                )        
         )
     )
 )
@@ -33,7 +53,7 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
     # Tracking the id's of the insert button
-    inserted = c()
+    inserted = c("A", "B","C","D")
     
     # Add entry row
     observeEvent(input$insertBtn, {
@@ -49,7 +69,7 @@ server <- function(input, output) {
         )
         insertUI(
             selector = '#excludes',
-            ui = textInput(paste0('exclude', btn), "can't get")
+            ui = textInput(paste0('exclude', btn), "can't get",value = "NA")
         )
         inserted <<- c(inserted, btn)
     })
@@ -78,6 +98,20 @@ server <- function(input, output) {
             emails = c(emails, input[[paste0("email",i)]])
             excludes = c(excludes, input[[paste0("exclude",i)]])
     }
+    names(emails) = names
+    name_combos = expand.grid(santa = names, recipient = names)
+    exclusion = data.frame(santa = names, exclude = excludes, stringsAsFactors = FALSE)
+    name_combos = merge(name_combos, exclusion)
+    print(name_combos)
+    name_combos = name_combos[name_combos$santa != name_combos$recipient,]
+    print(name_combos)
+    
+
+    # create function wrappe around grepl
+    # call this function with apply over name_combos recipient in exclude
+    
+
+    
     
     # send.mail(from = "secretsantabydrew@gmail.com",
     #           to = "drduber@gmail.com",
